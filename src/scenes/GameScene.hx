b52fcfaeb4227930b3313339b2716225ba06142f
roger <roger@elvex.org.ar>
2014-03-24 01:54:29 -0300

    add overlaytext enemies/kills

--- src/scenes/GameScene.hx
+++ src/scenes/GameScene.hx
@@ -1,5 +1,6 @@
 package scenes;
 
+import com.haxepunk.HXP;
 import com.haxepunk.Scene;
 import com.haxepunk.Entity;
 import com.haxepunk.utils.Input;
@@ -10,6 +11,8 @@ import entities.Enemy;
 
 import com.haxepunk.tmx.TmxEntity;
 import com.haxepunk.tmx.TmxMap;
+
+import com.haxepunk.graphics.Text;
 
 class GameScene extends Scene
 {
@@ -42,6 +45,7 @@ class GameScene extends Scene
             add(new Player(object.x, object.y));
         case "enemy":
             add(new Enemy(object.x, object.y));
+            numberEnemies++;
         default:
             trace("unknown type: " + object.type);
      }
@@ -50,9 +54,34 @@ class GameScene extends Scene
    add(e_map);
  }
 
+ private function addOverlay()
+ {
+    //overlayText = new Text(" = " + numberEnemies, 0, 0, 0, 0, { color:0x000000, size:30 } );
+    overlayText = new Text();
+    overlayText.resizable = true;
+    overlayText.color = 0x000000;
+    overlayText.size = 20;
+    overlayText.richText = "E: " + numberEnemies + " K: " + numberKills;
+    var overlay:Entity = new Entity(8, 8, overlayText);
+    add(overlay);
+ }
+
+ public override function remove<E:Entity>(entity:E):E
+ {
+     if(entity.type == "enemy") {
+        numberEnemies--;
+        numberKills++;
+        overlayText.richText = "E: " + numberEnemies + " K: " + numberKills;
+     }
+     return super.remove(entity);
+ }
 
  public override function begin()
  {
-     createMap();
+    createMap();
+    addOverlay();
  }
+ private var overlayText:Text;
+ private var numberEnemies:Int = 0;
+ private var numberKills:Int = 0;
 }
